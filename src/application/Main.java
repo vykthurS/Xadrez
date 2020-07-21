3f252e9ac40eec3574d4279fcb0b3ca042e56c79
vykthurS <vykthurrr@gmail.com>
2020-07-21 19:19:11 -0300

    Implementação tabuleiro e peças 0.1

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,11 +1,11 @@
 package application;
 
-import tabuleiro.Posicao;
+import tabuleiro.Tabul;
 
 public class Main {
 
 	public static void main(String[] args) {
-		
+		Tabul tabul=new Tabul(8,8);
 
 	}
 
