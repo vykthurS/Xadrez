0e2046af74f7b2c173a498fe9a17d05d3cf8e4cd
vykthurS <vykthurrr@gmail.com>
2020-07-30 18:44:48 -0300

    Movimentos possiveis do rei

--- src/xadrez/pecas/King.java
+++ src/xadrez/pecas/King.java
@@ -1,5 +1,6 @@
 package xadrez.pecas;
 
+import tabuleiro.Posicao;
 import tabuleiro.Tabul;
 import xadrez.Color;
 import xadrez.Peca_Xadrez;
@@ -13,9 +14,57 @@ public class King extends Peca_Xadrez {
 	public String toString() {
 		return"K";
 	}
+	
+	private boolean canMove(Posicao posicao) {
+		Peca_Xadrez p=(Peca_Xadrez)getTabu().peca(posicao);
+		return p ==null || p.getColor()!=getColor();
+	}
 	@Override
 	public boolean[][] movimentosPoss() {
 		boolean[][] mat=new boolean[getTabu().getLinhas()][getTabu().getColunas()];
+		
+		Posicao p= new Posicao(0,0);
+		
+		p.setValor(posicao.getLinha()-1, posicao.getColuna());
+		if(getTabu().existenciaPosicao(p)&& canMove(p)) {
+			mat[p.getLinha()][p.getColuna()]=true;
+		}
+		
+		p.setValor(posicao.getLinha()+1, posicao.getColuna());
+		if(getTabu().existenciaPosicao(p)&& canMove(p)) {
+			mat[p.getLinha()][p.getColuna()]=true;
+		}
+		
+		p.setValor(posicao.getLinha(), posicao.getColuna()-1);
+		if(getTabu().existenciaPosicao(p)&& canMove(p)) {
+			mat[p.getLinha()][p.getColuna()]=true;
+		}
+		
+		p.setValor(posicao.getLinha(), posicao.getColuna()+1);
+		if(getTabu().existenciaPosicao(p)&& canMove(p)) {
+			mat[p.getLinha()][p.getColuna()]=true;
+		}
+		p.setValor(posicao.getLinha()-1, posicao.getColuna()-1);
+		if(getTabu().existenciaPosicao(p)&& canMove(p)) {
+			mat[p.getLinha()][p.getColuna()]=true;
+		}
+		
+		p.setValor(posicao.getLinha()-1, posicao.getColuna()+1);
+		if(getTabu().existenciaPosicao(p)&& canMove(p)) {
+			mat[p.getLinha()][p.getColuna()]=true;
+		}
+		
+		p.setValor(posicao.getLinha()+1, posicao.getColuna()-1);
+		if(getTabu().existenciaPosicao(p)&& canMove(p)) {
+			mat[p.getLinha()][p.getColuna()]=true;
+		}
+		p.setValor(posicao.getLinha()+1, posicao.getColuna()+1);
+		if(getTabu().existenciaPosicao(p)&& canMove(p)) {
+			mat[p.getLinha()][p.getColuna()]=true;
+		}
+		
+		
+		
 		return mat;
 	}
 	
